6b2c183e42edc99b5290be326334a168c3300e27
CharlZ <charles.ceo2020@gmail.com>
2020-05-05 04:13:35 -0600

    footer addition

--- footer.php
+++ footer.php
@@ -1,2 +1,11 @@
+<footer>
+    <h2>Footer</h2>
+    <p>
+        &copy;
+        <?php echo date( 'Y' ); // Use PHP to keep the year current. Let it do the heavy lifting for you ?>
+        Charles on TechCareers Program by Manpower
+    </p>
+</footer>
+    <?php wp_footer(); ?>
 </body>
 </html>

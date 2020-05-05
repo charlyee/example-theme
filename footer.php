2890f7d64992520a30df7980e3a2b90b5ca8e1ab
CharlZ <charles.ceo2020@gmail.com>
2020-05-05 04:23:46 -0600

    theme creation in progress

--- footer.php
+++ footer.php
@@ -5,6 +5,11 @@
         <?php echo date( 'Y' ); // Use PHP to keep the year current. Let it do the heavy lifting for you ?>
         Charles on TechCareers Program by Manpower
     </p>
+    <?php if ( function_exists( 'output_hello_world' ) ) : // check if the plugin function exists ?>
+        <aside>
+            <?php output_hello_world(); ?>
+        </aside>
+    <?php endif; ?>
 </footer>
     <?php wp_footer(); ?>
 </body>
